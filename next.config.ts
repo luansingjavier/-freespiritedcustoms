81ac58becac5c1bb666e62b201736b1e34165bc3
Javier <luansingjavier@gmail.com>
2025-04-04 20:00:39 +0800

    fix: 404 not found

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  distDir: "out",
 };
 
 export default nextConfig;
